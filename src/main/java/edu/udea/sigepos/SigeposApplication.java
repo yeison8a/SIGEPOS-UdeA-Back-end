78dcc7b45fc2d4389b3374981027a7d5c22f7c40
yeison8a35 <yeison8a35@gmail.com>
2025-08-26 15:21:20 -0500

    first commit

---
+++ src/main/java/edu/udea/sigepos/SigeposApplication.java
@@ -0,0 +1,13 @@
+package edu.udea.sigepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigeposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigeposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udea/sigepos/SigeposApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udea.sigepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
